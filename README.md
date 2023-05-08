00c8d8466db97e76b901dda227485a1f1a44bd22
vladimir-laz <vladimir.lazarev.99@gmail.com>
2023-05-09 01:38:18 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Sentiment_ANALysis
+# Sentiment analysis
+
+This is a repository for a study project. 
+Here, we've implemented some of state-of-the-art approaches for sentiment analysis in russian language.
---
+++ models.py
@@ -0,0 +1,24 @@
+from torch import nn
+from transformers import BertModel
+
+class BertClassifier(nn.Module):
+    """
+    This classifier is using pretrained BERT model as a encoder and 
+    """
+    def __init__(self, dropout=0.5, num_classes=3):
+
+        super(BertClassifier, self).__init__()
+
+        self.bert = BertModel.from_pretrained('bert-base-cased')
+        self.dropout = nn.Dropout(dropout)
+        self.linear = nn.Linear(768, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, input_id, mask):
+
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = self.relu(linear_output)
+
+        return final_layer
